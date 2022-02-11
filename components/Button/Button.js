ae228189d748781a998091202477c18925ea11ce
Alyssa Palmares <alyssapalmares@yahoo.com>
2022-02-12 02:05:04 +0800

    added button

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,8 +1,20 @@
+import Link from 'next/link';
 import React from 'react';
 import styles from './Button.module.scss';
 
-const Button = () => {
-  return <div className={styles.container}>Hello Button</div>;
+const Button = ({children, variant = "purple", to = "#", className = ""}) => {
+  const color = {
+    purple: `${styles.purple}`,
+    white: `${styles.white} font-semibold`
+  };
+  
+  return (
+    <Link href={to}>
+      <a className={`${styles.container} py-2 px-8 max-h-14 max-w-12 text-base font-bold inline-block text-center h-auto rounded-md ${color[variant]} ${className}`}>
+        {children}
+      </a>
+    </Link>
+  );
 };
 
 export default Button;
--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,17 @@ export default function Home() {
     <Layout active={0}>
       <SEO title={"All Movies"} />
       <section className="px-32 py-2 mt-28 justify-center items-center">
+        {/* Header */}
         <h1 className="text-center text-6xl">All Movies</h1>
-          
+        
+        {/* Search and Add Movie */}
+        <div className='flex flex-row justify-between'>
+          {/* Search */}
+          <div className=''>
+
+          </div>
+          <Button variant="purple" to="/movies">Add Movie</Button>
+        </div>
       </section>
     </Layout>
   )
