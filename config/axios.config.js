e639fb140d9f1a4534f3bc826bf9045561a5b510
Gavin Raine Dizon <60138634+gavindizon@users.noreply.github.com>
2022-02-12 11:19:45 +0800

    changed url config

--- config/axios.config.js
+++ config/axios.config.js
@@ -6,6 +6,6 @@ import axios from "axios";
 const port = 8080;
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:" + port,
+    baseURL: "http://127.0.0.1:" + port,
     headers: { "Access-Control-Allow-Origin": "*" },
 });
